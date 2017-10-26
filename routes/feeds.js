817803608f8e0ccb71e1de7826890ed3377e8ee1
gaspard <gaspard@gmail.com>
2017-10-26 12:39:11 +0200

    first commit

---
+++ routes/feeds.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const i2a = require('../lib/instagramjson2atom');
+var request = require('request');
+
+/* GET users listing. */
+router.get('/:username', function(req, res, next) {
+  const sample = require('../lib/sample.json');
+  request('https://instagram.com/' + req.params.username + '/media', function (error, response, body) {
+    if (error) {
+      return res.send('unreachable');
+    }
+    let json = {}
+    try {
+      json = JSON.parse(body)
+    } catch (e) {
+      return res.send('unparsable');
+    }
+    return res.render('feed', {feed: i2a(json)});
+  });
+});
+
+module.exports = router;
---
+++ tests/instagramjson2atom.test.js
@@ -0,0 +1,4 @@
+const instagramjson2atom = require('../lib/instagramjson2atom');
+const jsonString = require('./mock.json');
+
+console.log(instagramjson2atom(jsonString));
