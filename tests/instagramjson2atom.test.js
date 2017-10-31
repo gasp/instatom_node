ba614a7ef4ef3a461fc2cd2ca178489b8d7cdc5a
gaspard <gaspard@gmail.com>
2017-10-31 14:22:57 +0100

    tests: add jasmine

--- tests/instagramjson2atom.test.js
+++ tests/instagramjson2atom.test.js
@@ -1,4 +1,12 @@
 const instagramjson2atom = require('../lib/instagramjson2atom');
 const jsonString = require('./mock.json');
 
-console.log(instagramjson2atom(jsonString));
+describe('instagram2json', () => {
+  const json = instagramjson2atom(jsonString);
+  it('parses all', () => {
+    expect(json.length).toBe(20);
+  })
+  it('has all elements', () => {
+    expect(Object.keys(json[0])).toEqual(['link','created_time','thumbnail_url','caption','username','full_name']);
+  })
+});
