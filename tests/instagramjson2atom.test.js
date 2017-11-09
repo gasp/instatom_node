b120123e3868771ccc6917a9601622574b07f540
gaspard <gaspard@gmail.com>
2017-11-09 11:50:11 +0100

    instagram new api: update tests

--- tests/instagramjson2atom.test.js
+++ tests/instagramjson2atom.test.js
@@ -3,10 +3,14 @@ const jsonString = require('./mock.json');
 
 describe('instagram2json', () => {
   const json = instagramjson2atom(jsonString);
+  const error = instagramjson2atom({user:{media:{nodes:[]}}});
   it('parses all', () => {
-    expect(json.length).toBe(20);
-  })
+    expect(json.length).toBe(12);
+  });
   it('has all elements', () => {
     expect(Object.keys(json[0])).toEqual(['link','created_time','thumbnail_url','caption','username','full_name']);
-  })
+  });
+  it('fails elegantly', () => {
+    expect(error).toEqual([]);
+  });
 });
