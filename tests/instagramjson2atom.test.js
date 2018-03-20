156e13c08be4888ac3868e385ec5b63cae013c0d
gaspard <gaspard@gmail.com>
2018-03-20 10:11:14 +0100

    i2a: fix captions (and more testing)

--- tests/instagramjson2atom.test.js
+++ tests/instagramjson2atom.test.js
@@ -23,6 +23,21 @@ describe('instagram2json', () => {
   it('has all elements', () => {
     expect(Object.keys(json[0])).toEqual(outputFields);
   });
+  it('has correct title', () => {
+    expect(json[5].caption).toBe('From the window of CCK');
+    expect(json[7].caption).toBe('\ud83d\udc08\ud83c\udf4a');
+  });
+  it('has correct name', () => {
+    expect(json[5].full_name).toBe('Gaspard');
+    expect(json[7].username).toBe('ryogasp');
+  });
+  it('has correct date', () => {
+    expect(json[5].created_time).toBe('2017-04-27T14:32:44Z');
+  });
+  it('has correct files', () => {
+    expect(json[5].thumbnail_url).toMatch(/^https/);
+    expect(json[5].link).toMatch(/_n\.jpg$/);
+  });
   it('fails elegantly', () => {
     expect(error).toEqual([]);
   });
